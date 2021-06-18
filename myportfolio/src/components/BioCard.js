a44895cbbb739601edef362c35df8d800d0244fb
Madison Vega <madison.r.vega@gmail.com>
2021-06-17 22:11:21 -0500

    added some formatting to biocard component with image

--- myportfolio/src/components/BioCard.js
+++ myportfolio/src/components/BioCard.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: 500,
+    marginTop: 25,
+    marginLeft: 25
   },
 });
+var imageStyle = {
+  display: 'block',
+  width: '30vw',
+  transitionDuration: '0.3s',
+  height: '35vw',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  marginTop: '25'
+
+}
 
 export default function BioCard() {
   const classes = useStyles();
@@ -21,30 +31,27 @@ export default function BioCard() {
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
+          style={imageStyle}
           component="img"
-          alt="Contemplative Reptile"
+          alt="MyPhoto"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          src="https://live.staticflickr.com/65535/50892027473_5d7bcf33ab_n.jpg"
+          title="MaddiV"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            About Me
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+            <p>
+              My name is Madison Vega, I am 26 years old, and I have lived all over the world, but
+              currently live in San Antonio, TX. I am presently working
+              in the financial industry and looking to change career paths. I am excited to be learning new skills in
+              programming and being the best that I can be.
+            </p>
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
     </Card>
   );
 }
